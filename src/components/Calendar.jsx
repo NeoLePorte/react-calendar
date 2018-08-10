e30f0a88702416d4719c6c17ae8e3e36a894e3f9
NeoLePorte <kagatookuda@gmail.com>
2018-08-10 19:21:55 -0400

    added calendar cells

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Grid } from 'semantic-ui-react'
+import { Grid, Header, Segment } from 'semantic-ui-react'
 import dateFns from "date-fns";
 
 export default class Calendar extends Component {
@@ -29,11 +29,48 @@ export default class Calendar extends Component {
         </React.Fragment>
         );
       }
+      //renders calendar days in cells
+      renderCells() {
+        const startDate = dateFns.startOfWeek(this.props.startDate);
+        const endDate = dateFns.addDays(this.props.startDate, this.props.numOfDays);
+        const dayDateFormat = "D";
+        let days = [];
+        let day = startDate;
+        let formattedDate = "";
+        
+        while (day <= endDate) {
+            for (let i = 0; i < 7; i++) {
+            formattedDate = dateFns.format(day, dayDateFormat);
+            // pushes calendar days to calendar
+                days.push(
+                    <Grid.Column key={day}>
+                        <Segment>
+                            {formattedDate}
+                        </Segment>
+                    </Grid.Column>
+                    )
+            day = dateFns.addDays(day, 1);
+            }
+        }
+        return (
+        <Grid.Row key={day}>
+          {days}
+        </Grid.Row>
+        )
+    }
     render() {
+        const headerDateFormat = "MMMM YYYY";
         return (
         <React.Fragment>
         <Grid columns={7} padded celled textAlign="center">
+        <Header 
+        textAlign="center" 
+        style={{marginTop: '1.5em'}}
+        >
+        {dateFns.format(this.props.startDate, headerDateFormat)}
+        </Header>
           {this.renderDays()}
+          {this.renderCells()}
         </Grid>
         </React.Fragment>
         )
