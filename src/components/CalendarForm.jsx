0be86eab2fc3163020fd3b6b17f5b800876f48e5
NeoLePorte <kagatookuda@gmail.com>
2018-08-10 17:30:43 -0400

    added form state

--- src/components/CalendarForm.jsx
+++ src/components/CalendarForm.jsx
@@ -1,6 +1,22 @@
 import React, { Component } from 'react'
+
 import {Form, Segment} from 'semantic-ui-react'
 export default class CalendarForm extends Component {
+  constructor() {
+    super()
+    this.state = {
+      startDate: Date(),
+      numOfDays: 0,
+      countryCode: ''
+    }
+  }
+  //saves input to state
+  onChangeText = (e) => {
+    const newState = { ...this.state };
+    newState[e.target.name] = e.target.value;
+    this.setState(newState);
+  }
+
 
   render() {
     return (
@@ -9,15 +25,15 @@ export default class CalendarForm extends Component {
       <Form widths="equal" size="small">
         <Form.Field>
             <label>Start Date</label>
-            <Form.Input name="startDate"  placeholder='Start Date' />
+            <Form.Input name="startDate" onChange={this.onChangeText} placeholder='Start Date' />
         </Form.Field>
         <Form.Field>
             <label>Number of Days</label>
-            <Form.Input name="numOfDays"  placeholder='Number of Days' />
+            <Form.Input name="numOfDays" onChange={this.onChangeText} placeholder='Number of Days' />
         </Form.Field>
         <Form.Field>
             <label>Country Code</label>
-            <Form.Input name="countryCode" placeholder='Country Code' />
+            <Form.Input name="countryCode" onChange={this.onChangeText} placeholder='Country Code' />
         </Form.Field>
       </Form>
       </ Segment>
